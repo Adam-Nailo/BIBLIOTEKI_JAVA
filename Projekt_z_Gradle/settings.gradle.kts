0cebcc7e2734399b73c25537a476c1127a6bac19
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-09 23:06:18 +0100

    initial commit

---
+++ Projekt_z_Gradle/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Projekt_z_Gradle"
+
---
+++ Projekt_z_Gradle/src/main/java/org/projekt_z_gradle/Main.java
@@ -0,0 +1,10 @@
+package org.projekt_z_gradle;
+
+/**
+* Created by Adam Seweryn
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

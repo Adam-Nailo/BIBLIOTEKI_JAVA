ccc32d01e9d6bf28623533795cf5efe426784674
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-10 21:13:29 +0100

    initial commit

---
+++ Projekt_z_Maven/src/main/java/org/projekt_z_maven/App.java
@@ -0,0 +1,18 @@
+package org.projekt_z_maven;
+
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+
+/**
+ * Hello world!
+ *
+ */
+@AllArgsConstructor
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new Gson();
+    }
+}

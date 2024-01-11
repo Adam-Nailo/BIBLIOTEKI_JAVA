6eef0f81a227af02298af1659b823861e7ccbbe6
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-11 20:15:00 +0100

    @Getter @Setter

---
+++ Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/ExampleModel.java
@@ -0,0 +1,19 @@
+package org.projekt_z_gradle;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Adam Seweryn
+ */
+public class ExampleModel {
+@Getter @Setter
+    private String hairColor;
+@Getter @Setter
+    private int hight;
+@Getter @Setter
+    private int width;
+@Getter @Setter
+    private boolean isMale;
+
+}
--- Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
+++ Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
@@ -1,14 +1,12 @@
 package org.projekt_z_gradle;
-
-import com.google.gson.Gson;
-import lombok.AllArgsConstructor;
-
 /**
  * Created by Adam Seweryn
  */
-@AllArgsConstructor
+
 public class Main {
+
     public static void main(String[] args) {
-        new Gson();
+ExampleModel exampleModel = new ExampleModel();
+// exampleModel.getHairColor();
     }
 }

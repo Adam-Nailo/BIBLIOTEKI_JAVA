826cda47035b1482c429e98de992530e248f699e
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-11 21:44:29 +0100

    @Data

--- Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/ExampleModel.java
+++ Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/ExampleModel.java
@@ -6,18 +6,11 @@ import lombok.*;
  * Created by Adam Seweryn
  */
 @Builder
-@Setter
-@Getter
-@ToString
-@EqualsAndHashCode
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class ExampleModel {
 private String hairColor;
 private int height;
 private int width;
 private boolean isMale;
 private int age;
-
-
 }

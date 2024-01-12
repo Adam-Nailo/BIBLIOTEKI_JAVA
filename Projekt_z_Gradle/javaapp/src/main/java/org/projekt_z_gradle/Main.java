4ef845ebe2811a5f565e48b29b5ad8423b6d027f
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-12 19:50:50 +0100

    GSON - zastosowanie

--- Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
+++ Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
@@ -8,23 +8,16 @@ import lombok.extern.java.Log;
  */
 @Log
 public class Main {
-
+public static final String json = "{\"hairColor\":\"Blond\",\"height\":185,\"width\":0,\"isMale\":true,\"age\":29}";
     public static void main(String[] args) {
         ExampleModel exampleModel = new ExampleModel("Blond", 185, 0, true, 29);
 
         System.out.println(exampleModel);
-
-        ExampleModel exampleModelDuplicate = ExampleModel.builder()
-                .age(29)
-                .hairColor("Blond")
-                .height(185)
-                .isMale(true)
-                .build();
-
-        System.out.println(exampleModelDuplicate);
+        String json = new Gson().toJson(exampleModel);
+        ExampleModel fromJsonExampleModel = new Gson().fromJson(json,ExampleModel.class);
 
         log.info("Huston, mamy problem!");
-        System.out.println(exampleModel.equals(exampleModelDuplicate));
+        System.out.println(exampleModel.equals(fromJsonExampleModel));
 
         new Gson();
     }

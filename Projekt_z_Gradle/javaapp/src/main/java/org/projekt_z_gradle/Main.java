c794d476084df6d58fbc95db214e59da3819d9db
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-15 09:44:57 +0100

    Wysylanie danych OkHttp

--- Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
+++ Projekt_z_Gradle/javaapp/src/main/java/org/projekt_z_gradle/Main.java
@@ -1,38 +1,31 @@
 package org.projekt_z_gradle;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import lombok.extern.java.Log;
 import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.List;
 
 /**
  * Created by Adam Seweryn
  */
 @Log
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://api.json-generator.com/templates/Lf9a60uCmqV4/data?access_token=cb6kj5xh2uj8qelvek8vyk9v1ssg78yeraasakd0")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                log.info(e.toString());
-            }
 
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                String json = response.body().string();
-                Type type = new TypeToken<List<Cloth>>(){}.getType();
-                List<Cloth> cloths = new Gson().fromJson(json,type);
-                log.info(cloths.get(0).toString());
-            }
-        });
-       log.info("koniec bloku");
+        RequestBody body = new FormBody.Builder()
+                .add("id", "1")
+                .add("title", "foo")
+                .add("body", "bar")
+                .add("userId", "1")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .headers(new Headers.Builder().add("Content-type", "application/json; charset=UTF-8").build())
+                .method("PUT", body)
+                .build();
+
+        Response response =client.newCall(request).execute();
+        log.info(response.body().string());
+        log.info("koniec bloku");
     }
 }
